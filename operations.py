03be7c581d799523a3bcd7dc2e687a9a7d5b81b9
mushtak <abc@gmail.com>
2025-02-10 22:23:10 +0530

    operations added

---
+++ operations.py
@@ -0,0 +1,13 @@
+def addition(a:int,b:int):
+	summation=a+b
+	return summation
+
+addition_re=addition(5,7)
+print(addition_re)
+
+def multiplication(a:int,b:int):
+	multiplication=a*b
+	return multiplication
+
+mul_re=multiplication(5,7)
+print(mul_re)
